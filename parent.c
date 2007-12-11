bb4f6210ac245db4893db390dbe8dbb16702b418
Shachar Shemesh <thesun@users.sourceforge.net>
2007-12-11 14:31:14 +0000

    Can decypher syscall parameters now

--- parent.c
+++ parent.c
@@ -3,10 +3,11 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/ptrace.h>
+#include <sys/syscall.h>
 
 #include <stdio.h>
 
-#include "platform.h"
+#include "arch/platform.h"
 
 int process_children(pid_t first_child)
 {
@@ -27,7 +28,16 @@ int process_children(pid_t first_child)
             int sig = WSTOPSIG(status);
             printf("Process %d halted due to signal %d\n", ret, sig );
             if( sig==SIGTRAP ) {
-                printf("SYSCALL %d captured\n", ptlib_get_syscall(ret));
+                int syscall=(int)ptlib_get_syscall(ret);
+                switch( syscall ) {
+                case __NR_write:
+                    printf("SYSCALL write(%d, %p, %d) called\n", (int)ptlib_get_argument(ret, 1), ptlib_get_argument(ret, 2),
+                        (int)ptlib_get_argument(ret, 3) );
+                    break;
+                default:
+                    printf("SYSCALL %d captured\n", syscall);
+                    break;
+                }
                 sig=0;
             } else {
                 printf("Process received signal %d\n", sig);
